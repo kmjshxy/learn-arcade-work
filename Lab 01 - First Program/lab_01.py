f708ce76528f6a8fcc478b5132b25b2834598f4b
Kate Minette <kate.minette@my.simpson.edu>
2021-09-03 11:58:06 -0500

    Work on lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,4 +1,4 @@
-print("hello my name is kate, \nI'm kansas city.")
+print("Hello my name is Kate. \nI'm kansas city.")
 print("I swim and I run track.")
 print("I don't have any experience with python. ")
-print("but i am excited to learn!!")
+print("I am excited to learn!!")
