3632101bbf07ed9e865886216ee82c7dbfba6f96
Kate Minette <kate.minette@my.simpson.edu>
2021-12-09 16:37:47 -0600

    final lab work

--- Lab 12 - Final Lab/part_12.py
+++ Lab 12 - Final Lab/part_12.py
@@ -1,15 +1,12 @@
 
 """
-Scroll around a large screen.
-
 Artwork from https://kenney.nl
-
-If Python and Arcade are installed, this example can be run from the command line with:
-python -m arcade.examples.sprite_move_scrolling
 """
 
+import datetime
 import random
 import arcade
+
 
 SPRITE_SCALING = 2.5
 CHARACTER_SCALING = 2.5
@@ -24,11 +21,13 @@ VIEWPORT_MARGIN = 220
 # How fast the camera pans to the player. 1.0 is instant.
 CAMERA_SPEED = 0.1
 
+
 # How fast the character moves
 PLAYER_MOVEMENT_SPEED = 7
-
 RIGHT_FACING = 0
 LEFT_FACING = 1
+
+
 
 def load_texture_pair(filename):
     """
@@ -36,7 +35,7 @@ def load_texture_pair(filename):
     """
     return [
         arcade.load_texture(filename),
-        arcade.load_texture(filename, flipped_horizontally=True)]
+        arcade.load_texture("plat1.png", flipped_horizontally=True)]
 
 
 class PlayerCharacter(arcade.Sprite):
@@ -48,32 +47,30 @@ class PlayerCharacter(arcade.Sprite):
         super().__init__()
 
         # Default to face-right
-        self.character_face_direction = RIGHT_FACING
+        self.character_face_direction = LEFT_FACING
 
         # Used for flipping between image sequences
         self.cur_texture = 0
         self.scale = CHARACTER_SCALING
 
         # Track our state
-        self.jumping = False
 
         # --- Load Textures ---
 
         # Images from Kenney.nl's Asset Pack 3
         main_path = "plat1.png"
 
-        # Load textures for idle standing
+        """ I drew the character texture"""
         self.idle_texture_pair = load_texture_pair("plat1.png")
-        # self.jump_texture_pair = load_texture_pair(f"{main_path}_jump.png")
-        # self.fall_texture_pair = load_texture_pair(f"{main_path}_fall.png")
+
+
 
         # Load textures for walking
         self.walk_textures = []
-        for i in range(8):
-            texture = load_texture_pair("plat2.png")
-            self.walk_textures.append(texture)
-            texture = load_texture_pair("plat3.png")
-            self.walk_textures.append(texture)
+        texture = load_texture_pair("plat2.png")
+        self.walk_textures.append(texture)
+        texture = load_texture_pair("plat3.png")
+        self.walk_textures.append(texture)
 
         # Load textures for climbing
 
@@ -88,34 +85,21 @@ class PlayerCharacter(arcade.Sprite):
     def update_animation(self, delta_time: float = 1 / 60):
 
         # Figure out if we need to flip face left or right
-        if self.change_x < 0 and self.character_face_direction == RIGHT_FACING:
+        if self.change_x > 0 and self.character_face_direction == RIGHT_FACING:
             self.character_face_direction = LEFT_FACING
         elif self.change_x > 0 and self.character_face_direction == LEFT_FACING:
             self.character_face_direction = RIGHT_FACING
 
-        # Climbing animation
-
-
-        # Jumping animation
-        # if self.change_y > 0:
-        #     self.texture = self.jump_texture_pair[self.character_face_direction]
-        #     return
-        # elif self.change_y < 0:
-        #     self.texture = self.fall_texture_pair[self.character_face_direction]
-        #     return
-
-        # Idle animation
-        if self.change_x == 0:
-            self.texture = self.idle_texture_pair[self.character_face_direction]
-            return
-
         # Walking animation
-        self.cur_texture += 1
-        if self.cur_texture > 7:
-            self.cur_texture = 0
-        self.texture = self.walk_textures[self.cur_texture][
-            self.character_face_direction
-        ]
+
+            self.cur_texture += 1
+            if self.cur_texture > 7:
+                self.cur_texture = 0
+            self.texture = self.walk_textures[self.cur_texture][
+                self.character_face_direction
+            ]
+
+
 class MyGame(arcade.Window):
     """ Main application class. """
 
@@ -128,6 +112,13 @@ class MyGame(arcade.Window):
         # Sprite lists
         self.player_list = None
         self.wall_list = None
+        self.rock_list = arcade.SpriteList()
+        self.holding_rn = 0
+        self.in_the_basket = 0
+        self.END = False
+
+        self.total_time = 0.0
+        self.output = "00:00:00"
 
         # Set up the player
         self.player_sprite = None
@@ -145,18 +136,27 @@ class MyGame(arcade.Window):
         # Sprite lists
         self.player_list = arcade.SpriteList()
         self.wall_list = arcade.SpriteList()
+        self.basket_list = arcade.SpriteList()
 
         # Set up the player
         self.player_sprite = PlayerCharacter()
-        self.player_sprite.center_x = 256
-        self.player_sprite.center_y = 512
+        self.player_sprite.center_x = 1000
+        self.player_sprite.center_y = 1200
         self.player_list.append(self.player_sprite)
+        """ pixel art gallery
+        basket 2
+        creator id: 6c4751
+        http://pixelartmaker.com/art/8eb55a4e52f96b7"""
+        self.basket = arcade.Sprite("basket.png", .3)
+        self.basket.center_x = 956
+        self.basket.center_y = 1295
+        self.basket_list.append(self.basket)
 
-        layer_options = {
-            "water": {
-                "use_spatial_hash": True,
-            },
-        }
+        self.total_time = 50
+
+
+
+
 
         map_name = "level1.json"
         self.tile_map = arcade.load_tilemap(map_name, scaling=.3, )
@@ -164,9 +164,30 @@ class MyGame(arcade.Window):
 
         self.scene = arcade.Scene.from_tilemap(self.tile_map)
 
+        rock_count = [[830,1245],
+                      [1560,1055],
+                      [1700, 360],
+                      [3000,515],
+                      [3100, 1360],
+                      [2510, 1400],
+                      [3750, 745],
+                      [3600, 1400],
+                      [3500, 975],
+                      [410, 400]]
+        for coordinate in rock_count:
+            """ pixel art gallery
+            rock 
+            creator id:9664c4"""
+            # http://pixelartmaker.com/art/384d668556ef0fc
+            rock = arcade.Sprite("rock.png", .1)
+
+            rock.center_x = coordinate[0]
+            rock.center_y = coordinate[1]
+
+            self.rock_list.append(rock)
 
         self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite, self.wall_list,
-                                                             gravity_constant=0.25)
+                                                             gravity_constant=0.35)
 
         if self.tile_map.background_color:
             arcade.set_background_color(self.tile_map.background_color)
@@ -183,9 +204,18 @@ class MyGame(arcade.Window):
         self.camera_sprites.use()
 
         # Draw all the sprites.
+        self.basket_list.draw()
         self.wall_list.draw()
         self.scene.draw()
+        self.rock_list.draw()
         self.player_list.draw()
+
+        arcade.draw_text("collect all the rocks before the time runs out!", 700, 1500, arcade.color.BLACK_BEAN,
+                         15, font_name='segoe print')
+        arcade.draw_text("you can only hold 4 rocks at a time tho...", 900, 1450, arcade.color.BLACK_BEAN,
+                         15, font_name= 'segoe print')
+        arcade.draw_text("use the arrow keys on your keyboard to move", 700, 1400, arcade.color.BLACK_BEAN,
+                         15, font_name='segoe print')
 
 
         # Select the (unscrolled) camera for our GUI
@@ -199,7 +229,25 @@ class MyGame(arcade.Window):
                                      arcade.color.ALMOND)
         text = f"Scroll value: ({self.camera_sprites.position[0]:5.1f}, " \
                f"{self.camera_sprites.position[1]:5.1f})"
-        arcade.draw_text(text, 10, 10, arcade.color.BLACK_BEAN, 20)
+
+        arcade.draw_text(f"holding: {self.holding_rn}", 10, 10, arcade.color.BLACK_BEAN, 20)
+        arcade.draw_text(f"in the basket: {self.in_the_basket}", 170, 10, arcade.color.BLACK_BEAN, 20)
+        arcade.draw_text(f"seconds left:{self.total_time}", 580, 10, arcade.color.BLACK_BEAN, 20)
+        if self.in_the_basket == 1:
+            self.END = True
+            arcade.draw_rectangle_filled(self.width // 2, 20, self.width, 8000, arcade.color.LIGHT_GREEN)
+            arcade.draw_text("CONGRATS YOU WON!!", 100, 300, arcade.color.BLACK_BEAN, 40)
+            arcade.draw_text(f"you collected all the rocks with {self.total_time} seconds to spare", 70, 250,
+                             arcade.color.BLACK_BEAN, 15)
+        if self.total_time <= 0:
+            arcade.draw_rectangle_filled(self.width // 2, 20, self.width, 8000, arcade.color.PINK)
+            arcade.draw_text("GAME OVER!", 250, 300, arcade.color.BLACK_BEAN, 40)
+            arcade.draw_text(f"you collected {self.in_the_basket} rocks. "
+                             f"only {10 - self.in_the_basket} rocks away!", 200, 250,
+                             arcade.color.BLACK_BEAN, 20)
+
+
+
 
     def prossess_keychange(self):
         # Process left/right
@@ -215,7 +263,7 @@ class MyGame(arcade.Window):
 
         if key == arcade.key.UP:
             if self.physics_engine.can_jump():
-                self.player_sprite.change_y = 6
+                self.player_sprite.change_y = 9
         elif key == arcade.key.LEFT:
             self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
         elif key == arcade.key.RIGHT:
@@ -240,6 +288,38 @@ class MyGame(arcade.Window):
 
         # Scroll the screen to the player
         self.scroll_to_player()
+
+        self.rock_list.update()
+
+        rock_pick_up_list = arcade.check_for_collision_with_list(self.player_sprite, self.rock_list)
+        for rock in rock_pick_up_list:
+            if self.holding_rn <= 3:
+                rock.remove_from_sprite_lists()
+                self.holding_rn += 1
+        placed_in_the_basket = arcade.check_for_collision_with_list(self.player_sprite, self.basket_list)
+        if placed_in_the_basket:
+            self.in_the_basket += self.holding_rn
+            self.holding_rn = 0
+
+        if self.END == False:
+            self.total_time -= delta_time
+
+            # Calculate minutes
+            minutes = int(self.total_time) // 60
+
+            # Calculate seconds by using a modulus (remainder)
+            seconds = int(self.total_time) % 60
+
+            # Calculate 100s of a second
+            seconds_100s = int((self.total_time - seconds) * 100)
+
+            # Figure out our output
+            self.output = f"{minutes:02d}:{seconds:02d}"
+
+
+
+
+
 
 
 
@@ -270,6 +350,8 @@ def main():
     window = MyGame(DEFAULT_SCREEN_WIDTH, DEFAULT_SCREEN_HEIGHT, SCREEN_TITLE)
     window.setup()
     arcade.run()
+
+
 
 
 if __name__ == "__main__":
